adf0d4a65d680695b3b0ee16969186df8b2bc437
Terrev <21133460+Terrev@users.noreply.github.com>
2020-01-10 02:12:39 -0700

    shrug

--- LWO-to-OBJ/Properties/AssemblyInfo.cs
+++ LWO-to-OBJ/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("LWO-to-OBJ")]
+[assembly: AssemblyTitle("LWO to OBJ")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("LWO-to-OBJ")]
+[assembly: AssemblyProduct("LWO to OBJ")]
 [assembly: AssemblyCopyright("Copyright ©  2020")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
